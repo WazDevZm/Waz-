d2f62736f79c7b84c32000c17b83f763ea4e2b2f
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-02-12 22:40:54 +0200

    added dep

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.requests import Request
 
-from 
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+
+DATABASE_URL = ""
+
+
+
+engine =create_engine(DATABASE)
