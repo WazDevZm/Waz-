644efd1fe136d308f9b6b7e18a4310b23db756fa
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-02-13 23:37:00 +0200

    Added soem more files

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import ollama
 
 class WazDevVoiceAssistant:
     def __init__(self):
-        aai.settings.api_key = "Wazingwa"
+        aai.settings.api_key = "3a51972ec82e41fb95c0a8865fb3a82d"
         self.client = ElevenLabs(
-            api_key= "Wazingwa"
+            api_key= "3d4cbcbeb7dd428b8b429eeb6ad8455b"
         )
         
         self.transcriber = None
@@ -36,4 +36,65 @@ class WazDevVoiceAssistant:
             self.transcriber.close()
             self.transcriber = None
     
-    def on_open()
+    def on_open(self, session_opened: aai.RealtimeSessionOpened):
+        #print("Session ID:", session_opened.session_id)
+        return
+    
+    def on_data(self, transcript: aai.RealtimeTranscript):
+        if not transcript.text:
+            return
+
+        if isinstance(transcript, aai.RealtimeFinalTranscript):
+            print(transcript.text)
+            self.generate_ai_response(transcript)
+        else:
+            print(transcript.text, end="\r")
+
+    def on_error(self, error: aai.RealtimeError):
+        #print("An error occured:", error)
+        return
+
+    def on_close(self):
+        #print("Closing Session")
+        return    
+    
+    def generate_ai_response(self, transcript):
+        self.stop_transcription()
+
+        self.full_transcript.append({"role":"user", "content":transcript.text})
+        print(f"\nUser:{transcript.text}", end="\r\n")
+
+        ollama_stream = ollama.chat(
+            model = "deepseek-r1:7b",
+            messages = self.full_transcript,
+            stream = True,
+        )
+
+        print("DeepSeek R1:", end="\r\n")
+        text_buffer = ""
+        full_text = ""
+        for chunk in ollama_stream:
+            text_buffer += chunk['message']['content']
+            if text_buffer.endswith('.'):
+                audio_stream = self.client.generate(text=text_buffer,
+                                                    model="eleven_turbo_v2",
+                                                    stream=True)
+                print(text_buffer, end="\n", flush=True)
+                stream(audio_stream)
+                full_text += text_buffer
+                text_buffer = ""
+
+        if text_buffer:
+            audio_stream = self.client.generate(text=text_buffer,
+                                                    model="eleven_turbo_v2",
+                                                    stream=True)
+            print(text_buffer, end="\n", flush=True)
+            stream(audio_stream)
+            full_text += text_buffer
+
+        self.full_transcript.append({"role":"assistant", "content":full_text})
+
+        self.start_transcription()
+
+ai_voice_agent = WazDevVoiceAssistant()
+ai_voice_agent.start_transcription()
